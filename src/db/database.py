09eb37f1f6308ea46cadc8f218d7713bdb55de11
xhackerfinnx <hfalex120@mail.ru>
2025-01-17 00:05:33 +0300

    Файл для работы с базой данных

---
+++ src/db/database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.db_path = 'src/db/base/bitcoin_transaction.db'
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
+        self.create_tables()
+
+    def create_tables(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS transactions (
+                hash TEXT,
+                type_wallet TEXT,
+                wallet TEXT,
+                volume INTEGER,
+                datetime TEXT
+            );
+        """)
+        self.connection.commit()
+        
+    def execute_query(self, query, params):
+        self.cursor.execute(query, params)
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
